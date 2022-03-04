0ba23e74bae813d1b42863b9d241ba492e218622
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-03-04 14:28:11 +0700

    Update igstalk.js

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -35,6 +35,7 @@ ${biography}${external_url ? '\n*External URL:* ' + external_url : ''}
 }
 handler.help = ['igstalk'].map(v => v + ' *username*')
 handler.tags = ['downloader']
+handler.premium = true
 handler.limit = true
 handler.command = /^(igstalk)$/i
 

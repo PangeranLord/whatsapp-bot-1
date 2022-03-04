b8e8ec304d052b2dbe2f09fc641ffb0ae4d1e476
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-03-04 15:23:46 +0700

    Update unbanchat.js

--- plugins/unbanchat.js
+++ plugins/unbanchat.js
@@ -1,23 +1,10 @@
-let handler = async (m, { conn, isOwner, text, isAdmin }) => {
-  let who
-  if (m.isGroup) {
-    if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
-    who = m.mentionedJid[0] ? m.mentionedJid[0] : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
-    // else who = m.chat
-  } else {
-    if (!isOwner) return dfail('owner', m, conn)
-    who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
-  }
-  try {
-    if (who.endsWith('g.us')) db.data.chats[who].isBanned = false
-    else db.data.users[who].banned = false
-    m.reply(`${conn.user.name} sekarang aktif dichat ${conn.getName(who) == undefined ? 'ini' : conn.getName(who)}.`)
-  } catch (e) {
-    throw `nomor tidak ada di database!`
-  }
+let handler = async (m, { conn }) => {
+  global.db.data.chats[m.chat].isBanned = false
+  m.reply('Done!')
 }
-handler.help = ['unban']
-handler.tags = ['owner', 'group']
-handler.command = /^unban(chat)?$/i
+handler.help = ['unbanchat']
+handler.tags = ['owner']
+handler.command = /^unbanchat$/i
+handler.owner = true
 
 module.exports = handler

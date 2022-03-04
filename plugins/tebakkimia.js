fa1434686393bc81cdf7c6976a1a74ac3659ce96
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-03-04 15:49:09 +0700

    Update tebakkimia.js

--- plugins/tebakkimia.js
+++ plugins/tebakkimia.js
@@ -19,10 +19,10 @@ Ketik ${usedPrefix}teki untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.tebakkimia[id] = [
-        await conn.sendButton(m.chat, caption, '© sekha', 'Bantuan', '.teki', m),
+        await conn.sendButton(m.chat, caption, wm, 'Bantuan', '.teki', m),
         json, poin,
         setTimeout(async () => {
-            if (conn.tebakkimia[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.unsur}*`, '© sekha', 'Tebak Kimia', '.tebakkimia', conn.tebakkimia[id][0])
+            if (conn.tebakkimia[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.unsur}*`, wm, 'Tebak Kimia', '.tebakkimia', conn.tebakkimia[id][0])
             delete conn.tebakkimia[id]
         }, timeout)
     ]
